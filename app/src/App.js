4a198bd033f85d64842644d2cdd54bdca49af580
Hossein Dehnokhalaji <sc16h2d@leeds.ac.uk>
2018-06-09 23:38:05 +0100

    made compatible with this project

--- app/src/App.js
+++ app/src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">After Me</h1>
         </header>
         <p className="App-intro">{this.state.response}</p>
       </div>
